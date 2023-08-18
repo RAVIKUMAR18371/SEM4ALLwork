dfcaef00770945953f989912ef3b42d7e6d10ee2
RAVIKUMAR18371 <ravikumarparasar31@gmail.com>
2023-08-18 11:26:53 +0530

    First Commit

---
+++ src/Leetcode/sum_3.java
@@ -0,0 +1,48 @@
+package Leetcode;
+/*
+import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class sum_3 {
+    static class Edge{
+        int i;
+        int j;
+        int k;
+        Edge(int i , int j , int k){
+            this.i = i;
+            this.j = j;
+            this.k = k;
+        }
+    }
+    public static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> LL = new List<>() {
+
+        int p = 0;
+        for(int i = 0; i < nums.length - 2; i++){
+            for(int j = i+1; j < nums.length - 1; j++){
+                for(int k = i+2; k < nums.length; k++){
+                    int s = p++;
+                    if(nums[i] + nums[j] + nums[k] == 0) {
+                        al[s].add(new Edge(nums[i],nums[j],nums[k]));
+                    }
+                }
+            }
+        }
+        return al;
+    }
+        public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array:");
+        int n = sc.nextInt();
+        System.out.println("Enter the elements in the array:");
+        int[] nums = new int[n];
+        for(int i = 0; i < n; i++){
+            nums[i] = sc.nextInt();
+        }
+            System.out.println(Arrays.toString(threeSum(nums)));
+
+    }
+}
+
+ */
